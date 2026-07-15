86083f68fe4315d4e5194ffe6c25c965eca5e64a
pranawk <jiopranaw@gmail.com>
2026-07-15 17:21:04 +0530

    cp codes

--- Round 1109/B_Nikita_and_Books.cpp
+++ Round 1109/B_Nikita_and_Books.cpp
@@ -10,7 +10,24 @@ int main(){
     int tt;
     cin>>tt;
     while(tt--){
-        
+        int n;
+        cin>>n;
+        vector<int>a(n);
+        for(int i=0; i<n; i++)cin>>a[i];
+        long long mor=0;
+        bool fl=true;
+        int ii=1;
+        for(int i=0; i<n; i++){
+            if(a[i]<ii){
+                if(a[i]+mor<ii){fl=false;break;}
+                mor-=ii-a[i];
+            }
+            else{
+                mor+=a[i]-ii;
+            }
+            ii++;
+        }
+        cout<<(fl==true ?"YES":"NO")<<endl;
     }
     return 0;
 }
---
+++ Round 1109/C_Stepan_and_Permutation.cpp
@@ -0,0 +1,31 @@
+//  C. Stepan and Permutation
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tt;
+    cin>>tt;
+    while(tt--){
+        int n,x,y;
+        cin>>n>>x>>y;
+        vector<int>a(n);
+        for(int i=0; i<n; i++)cin>>a[i];
+        bool fl=true;
+        vector<bool>dp(n+1,false);
+        dp[0]=true;
+        for(int i=x; i<=n; i++)if(dp[i-x]==true)dp[i]=true;
+        for(int i=y; i<=n; i++)if(dp[i-y]==true)dp[i]=true;
+        int dif=abs(x-y);
+        for(int i=dif; i<=dif; i++)if(dp[i-dif]==true)dp[i]=true;
+        for(int i=0; i<n; i++){
+            dif=abs(a[i]-i-1);
+            if(dp[dif]==false){fl=false;continue;}
+        }
+        cout<<(fl==true ? "YES" : "NO")<<endl;
+    }
+    return 0;
+}
