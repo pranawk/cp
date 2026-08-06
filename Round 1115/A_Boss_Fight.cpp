5f3cf7eb66c451916fa275869f2504fea1e59c50
pranawk <jiopranaw@gmail.com>
2026-08-06 23:05:45 +0530

    cp codes

--- Round 1115/A_Boss_Fight.cpp
+++ Round 1115/A_Boss_Fight.cpp
@@ -12,11 +12,19 @@ int main(){
     while(tt--){
         int n;
         cin>>n;
-        vector<int>a(n);
-        for(int i=0; i<n; i++){cin>>a[i];}
-        sort(a.begin(),a.end());
+        vector<int>a;
+        unordered_map<int,int>mp;
+        int temp;
+        for(int i=0; i<n; i++){cin>>temp;mp[temp]++;}
+        vector<pair<int,int>>pp;
+        for(auto i: mp)pp.push_back({i.second,i.first});
+        sort(pp.begin(),pp.end());
+        for(int i=0; i<pp.size(); i++){
+            for(int j=0; j<pp[i].first; j++)a.push_back(pp[i].second);
+        }
         int l=0,r=n-1;
         int ans=0;
+        //for(int i=0; i<n; i++)cout<<a[i]<<" ";
         while(l<=r){
             if(l==r){
                 ans+=a[l];break;
