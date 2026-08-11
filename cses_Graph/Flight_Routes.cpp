934d657fb3988b7e4808a08e93e314affb18725e
pranawk <jiopranaw@gmail.com>
2026-08-11 23:37:24 +0530

    cp codes

--- cses_Graph/Flight_Routes.cpp
+++ cses_Graph/Flight_Routes.cpp
@@ -15,6 +15,29 @@ int main(){
         cin>>t1>>t2>>t3;
         adj[t1].push_back({t2,t3});
     }
+    priority_queue<pair<int,int>,vector<pair<int,int>>, greater<pair<int,int>>>pq;
+    pq.push({1,0});
+    vector<vector<int>>mins(n+1);
+    int nn=n*k;
+    while(nn-- && !pq.empty()){
+        int top=pq.top().first;
+        int cc=pq.top().second;
+        pq.pop();
+        priority_queue<int,vector<int>,greater<int>>temp;
+        for(int i=0; i<mins[top].size(); i++)temp.push(mins[top][i]);
+        vector<int>tp;int aa=k;
+        while(aa-- && !temp.empty()){
+            tp.push_back(temp.top());
+            temp.pop();
+        }
+        mins[top]=tp;
+        for(int i=0; i<adj[top].size(); i++){
+            pq.push({adj[top][i].first, cc+adj[top][i].second});
+            mins[adj[top][i].first].push_back(cc+adj[top][i].second);
+        }
+    }
+    sort(mins[n].begin(),mins[n].end());
+    for(int i=0; i<k && i<mins[n].size(); i++)cout<<mins[n][i]<<" ";
 
     return 0;
 }
