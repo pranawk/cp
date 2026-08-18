30dc6510c0685df8eb6dbd8d021668a9e0f99a05
pranawk <jiopranaw@gmail.com>
2026-08-18 09:14:54 +0530

    cp codes

--- Round 1117/C_Spying_on_the_Beaver.cpp
+++ Round 1117/C_Spying_on_the_Beaver.cpp
@@ -1,107 +1,47 @@
-#include <bits/stdc++.h>
+//  C. Spying on the Beaver
+
+#include<bits/stdc++.h>
+
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int n;
-    cin >> n;
-
-    vector<vector<int>> children(n + 1);
-    for (int i = 2; i <= n; i++) {
-        int p;
-        cin >> p;
-        children[p].push_back(i);
-    }
-
-    int m;
-    cin >> m;
-    vector<bool> isDam(n + 1, false);
-    for (int i = 0; i < m; i++) {
-        int x;
-        cin >> x;
-        isDam[x] = true;
-    }
-
-    // Postorder traversal to compute which subtrees contain dams
-    vector<int> order;
-    order.reserve(n);
-    vector<int> stack = {1};
-    while (!stack.empty()) {
-        int u = stack.back();
-        stack.pop_back();
-        order.push_back(u);
-        for (int v : children[u]) stack.push_back(v);
-    }
-    reverse(order.begin(), order.end());
-
-    vector<bool> hasDam(n + 1, false);
-    for (int u : order) {
-        bool damHere = isDam[u];
-        for (int v : children[u]) {
-            damHere = damHere || hasDam[v];
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tt;
+    cin >> tt;
+    while (tt--)
+    {
+        int n;
+        cin >> n;
+        vector<int> parent(n + 1, -1);
+        vector<int> depth(n + 1, 0);
+        depth[1] = 0;
+        vector<bool> con(n + 1, false);
+        con[1] = true;
+        for (int i = 0; i < n - 1; i++)
+        {
+            int temp;
+            cin >> temp;
+            depth[i + 2] = depth[temp] + 1;
+            if (con[temp] == true)con[i + 2] = true;
+            parent[i + 2] = temp;
         }
-        hasDam[u] = damHere;
-    }
-
-    // DP to compute total cameras (optional, can also count during reconstruction)
-    vector<int> dp(n + 1, 0);
-    for (int u : order) {
-        int cnt = 0;
-        int sum = 0;
-        for (int v : children[u]) {
-            if (hasDam[v]) {
-                cnt++;
-                sum += dp[v];
-            }
+        int m;
+        cin >> m;
+        vector<int> cm(m);
+        for (int i = 0; i < m; i++)cin >> cm[i];
+        if (m == 1)
+        {
+            cout << 0 << endl;
+            continue;
         }
-        if (isDam[u]) {
-            dp[u] = cnt + sum;
-        } else {
-            dp[u] = (cnt == 0 ? 0 : (cnt - 1)) + sum;
-        }
+        cout << m - 1 << " ";
+        vector<pair<int, int>> ans;
+        for (int i = 0; i < m; i++) { ans.push_back({depth[cm[i]], cm[i]}); }
+        sort(ans.begin(), ans.end());
+        for (int i = 1; i < ans.size(); i++)cout << ans[i].second << " ";
+        cout << endl;
     }
-
-    // Reconstruction of edges
-    vector<pair<int, int>> edges;
-    vector<int> nodes = {1};
-    while (!nodes.empty()) {
-        int u = nodes.back();
-        nodes.pop_back();
-
-        vector<int> damChildren;
-        for (int v : children[u]) {
-            if (hasDam[v]) damChildren.push_back(v);
-        }
-
-        if (isDam[u]) {
-            // place cameras on all edges to children that contain dams
-            for (int v : damChildren) {
-                edges.push_back({u, v});
-                nodes.push_back(v);
-            }
-        } else {
-            if (damChildren.size() >= 2) {
-                // place on all except the first one
-                for (int i = 1; i < (int)damChildren.size(); i++) {
-                    edges.push_back({u, damChildren[i]});
-                    nodes.push_back(damChildren[i]);
-                }
-                // also process the first child (no edge at this level)
-                nodes.push_back(damChildren[0]);
-            } else if (damChildren.size() == 1) {
-                // only one child – no camera on this edge, just go deeper
-                nodes.push_back(damChildren[0]);
-            }
-            // if none, nothing to do
-        }
-    }
-
-    cout << dp[1] << '\n';
-    for (auto [u, v] : edges) {
-        cout << u << ' ' << v << '\n';
-    }
-
     return 0;
 }
