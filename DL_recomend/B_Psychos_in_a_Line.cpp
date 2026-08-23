00dd9a7c6ff2f8ff3dd94ac00a80d8f77bf5e767
pranawk <jiopranaw@gmail.com>
2026-08-23 23:17:08 +0530

    cp codes

--- DL_recomend/B_Psychos_in_a_Line.cpp
+++ DL_recomend/B_Psychos_in_a_Line.cpp
@@ -4,29 +4,30 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int n;
-    cin>>n;
-    vector<int>a(n);
-    for (int i=0; i<n; i++)cin>>a[i];
-    vector<int>ans(n,0);
-    vector<int>life(n,INT_MAX);
-    stack<pair<int,int>>st;
-    for (int i=0; i<n; i++)
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)cin >> a[i];
+    vector<int> ans(n, 0);
+    vector<int> life(n,INT_MAX);
+    stack<pair<int, int>> st;
+    for (int i = 0; i < n; i++)
     {
-        while (!st.empty() && (st.top().first<a[i]|| life[st.top().second]<=ans[st.top().second]))st.pop();
+        while (!st.empty() && (st.top().first < a[i] || life[st.top().second] <= ans[st.top().second]))st.pop();
         if (!st.empty())
         {
             ans[st.top().second]++;
-            life[i]=ans[st.top().second];
+            life[i] = ans[st.top().second];
         }
-        st.push({a[i],i});
+        st.push({a[i], i});
     }
     // for (int i=0; i<n; i++)cout<<life[i]<<" ";
     // cout<<endl;
     // for (int i=0; i<n; i++)cout<<ans[i]<<" ";
-    cout<<*max_element(ans.begin(),ans.end());
+    cout << *max_element(ans.begin(), ans.end());
     return 0;
 }
