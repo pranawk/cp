89abf3189e9e928f7b9e6385fb1b732c2dffff2a
pranawk <jiopranaw@gmail.com>
2026-09-03 23:51:25 +0530

    cp codes

--- A2DP/C_Hard_Process.cpp
+++ A2DP/C_Hard_Process.cpp
@@ -7,10 +7,35 @@ using namespace std;
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int tt;
-    cin>>tt;
-    while(tt--){
-        
+    int n,k;
+    cin>>n>>k;
+    vector<int>a(n);
+    for (int i=0; i<n; i++)cin>>a[i];
+    int l=0,r=0;
+    int ci=0, al=0,ar=-1;
+    while (r<n)
+    {
+        if (a[r]==0)ci++;
+        if (ci>k)
+        {
+            if (ar-al< r-1-l)
+            {
+                al=l; ar=r-1;
+            }
+            while (ci>k)
+            {
+                if (a[l]==0)ci--;
+                l++;
+            }
+        }
+        r++;
+    }
+    if (ar-al<r-l-1){al=l;ar=r-1;}
+    cout<<ar-al+1<<endl;
+    for (int i=0; i<n; i++)
+    {
+        if (i>=al && i<=ar)cout<<1<<" ";
+        else cout<<a[i]<<" ";
     }
     return 0;
 }
