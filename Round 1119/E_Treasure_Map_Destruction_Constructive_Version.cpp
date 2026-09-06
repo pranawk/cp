52da2e678b626c566538324ff78ffd3b72237192
pranawk <jiopranaw@gmail.com>
2026-09-07 00:05:58 +0530

    cp codes

--- Round 1119/E_Treasure_Map_Destruction_Constructive_Version.cpp
+++ Round 1119/E_Treasure_Map_Destruction_Constructive_Version.cpp
@@ -44,10 +44,25 @@ int main()
         int cn=0;
         for (int i=0; i<n; i++)
         {
-            if (dz[i]==0 && (a[i]==0 || a[i]==-1)){ans+="1";cn++;}
+            if (dz[i]==0 && ( a[i]==-1)){a[i]=0;}
+        }
+        for (int i=0; i<n; i++)
+        {
+            if (a[i]!=0 && a[i]!=-1)
+            {
+                bool f1=false,f2=false;
+                if ((i-a[i]>=0 && a[i-a[i]]==0))f1=true;
+                if (i+a[i]<n && a[i+a[i]]==0)f2=true;
+                fl&=(f1|f2);
+            }
+            if (a[i]==0)cn++;
+        }
+        if (cn==0 || fl==false){cout<<-1<<endl;continue;}
+        for (int i=0; i<n; i++)
+        {
+            if (a[i]==0)ans+="1";
             else ans+="0";
         }
-        if (cn==0){cout<<-1<<endl;continue;}
         cout<<ans<<endl;
     }
     return 0;
