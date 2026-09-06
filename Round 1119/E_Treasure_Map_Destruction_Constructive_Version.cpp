50f4a732092867a56a03231e3f84dfc5f59303e9
pranawk <jiopranaw@gmail.com>
2026-09-06 23:51:30 +0530

    cp codes

--- Round 1119/E_Treasure_Map_Destruction_Constructive_Version.cpp
+++ Round 1119/E_Treasure_Map_Destruction_Constructive_Version.cpp
@@ -16,84 +16,39 @@ int main()
         cin>>n;
         vector<int>a(n);
         for (int i=0; i<n; i++)cin>>a[i];
-        vector<pair<int,int>>pts;
-        for (int i=0; i<n; i++)if (a[i]!=-1)pts.push_back({a[i],i});
-        bool fl=true;
-        for (int i=1; i<pts.size(); i++)
+        vector<int>dz(n,0);
+        for (int i=0; i<n; i++)
         {
-            int dif=abs(pts[i].first-pts[i-1].first);
-            if (dif>pts[i].second-pts[i-1].second)
+            if (a[i]!=0 && a[i]!=-1)
             {
-                fl=false;break;
-            }
-            // if (pts[i].first>=pts[i-1].first)
-            // {
-                int j,k;
-                for (j=pts[i].second-1; j>pts[i-1].second; j--)
-                {
-                    if (a[j+1]==0){j++;break;}
-                    a[j]=a[j+1]-1;
-                    if (a[j]==0)break;
-                }
-                for (k=pts[i-1].second+1; k<=j; k++)
-                {
-                    a[k]=a[k-1]-1;
-                    if (a[k]==0)break;
-                }
-                // k++;
-                // if (j-1!=k && (j-k)%2==0){a[k]=1;k++;}
-                bool bb=true;
-                // cout<<j<<" "<<k<<endl;
-                for (int l=k+1; l<j; l++)
-                {
-                    // if (l>=j)break;
-                    if (bb==true)a[l]=1;
-                    else a[l]=0;
-                    bb^=1;
-                }
-            // }
-        }
-        if (pts.size()!=0)
-        {
-            int dif=1;
-            for (int i=pts[0].second-1; i>=0; i--)
-            {
-                if (a[i+1]==0)dif=-1;
-                a[i]=a[i+1]-dif;
-            }
-            dif=1;
-            for (int i=pts[pts.size()-1].second+1; i<n; i++)
-            {
-                if (a[i-1]==0)dif=-1;
-                a[i]=a[i-1]-dif;
+                if (i-a[i]+1>=0)dz[i-a[i]+1]++;
+                else dz[0]++;
+                if (i+a[i]<n)dz[i+a[i]]--;
             }
         }
-        else
-        {
-            for (int i=0; i<n; i++)a[i]=i;
-        }
-        for (int i=0; i<n; i++)cout<<a[i]<<" ";
-        string ans;
-        bool desc;
-        if (n>1&& a[1]>a[0])desc=false;
-        if (a[0]==0)ans="1";
-        else ans="0";
         for (int i=1; i<n; i++)
         {
-           char c;
-            if (a[i]==0){desc=true;c='1';}
-            else
-            {
-                if (a[i]==a[i-1] && (a[i]!=0||a[i]!=1))fl=false;
-                if (desc==true && a[i]-a[i-1]>1)fl=false;
-                else if (desc==false && a[i-1]-a[i]>1)fl=false;
-                c='0';
-            }
-            ans.push_back(c);
+            dz[i]+=dz[i-1];
         }
-        if (fl==false)cout<<"-1";
-        else cout<<ans;
-        cout<<endl;
+        bool fl=true;
+        for (int i=0; i<n; i++)
+        {
+            if (dz[i]>0 && a[i]==0)fl=false;
+        }
+        if (fl==false)
+        {
+            cout<<-1<<endl;
+            continue;
+        }
+        string ans;
+        int cn=0;
+        for (int i=0; i<n; i++)
+        {
+            if (dz[i]==0 && (a[i]==0 || a[i]==-1)){ans+="1";cn++;}
+            else ans+="0";
+        }
+        if (cn==0){cout<<-1<<endl;continue;}
+        cout<<ans<<endl;
     }
     return 0;
 }
