6d42c8a18039fa718a7b72353098920e417c20e4
pranawk <jiopranaw@gmail.com>
2026-09-09 22:11:12 +0530

    cp codes

--- Educational 194/B_Monocarp_and_Projects.cpp
+++ Educational 194/B_Monocarp_and_Projects.cpp
@@ -2,26 +2,7 @@
 
 #include<bits/stdc++.h>
 using namespace std;
-long long solve(long long x, long long y, long long n) {
-    if (y < x) {
-        return (n + 1) * y + (n * (n + 1)) / 2;
-    }
-    long long diff = y - x;
-    long long pp = y - 2 * x;
-    long long total = 0;
-    if (pp < 0) {
-        return diff * (n + 1);
-    }
-    long long limit =min(pp, n);
-    for (long long i = 0; i <= limit; ++i) {
-        total += diff % (x + i);
-    }
-    if (n > pp) {
-        long long rt= n - pp;
-        total += rt* diff;
-    }
-    return total;
-}
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -30,8 +11,17 @@ int main(){
     while(tt--){
         long long x,y,k;
         cin>>x>>y>>k;
-        cout<<solve(x,y,k-1)<<endl;
+        long long ans=0;
+        long long i;
+        for (i=0;  i<=k-1 && i<=(y-2*x); i++)
+        {
+            ans+=(y-x)%(x+i);
+        }
+        if (k-1>(y-2*x))
+        {
+            ans+=1ll*(k-1-y+2*x)*(y-x);
+        }
+        cout<<ans<<endl;
     }
     return 0;
 }
-
